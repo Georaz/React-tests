26e41ca855896981cbaab2cadfb2e12465572968
Georaz <46198196+Georaz@users.noreply.github.com>
2021-01-12 22:59:15 +0300

    Add files via upload
    Добавляет файлы папки src.

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react'
+import Layout from './hoc/Layout/Layout.js'
+import {Route, Switch, Redirect, withRouter} from 'react-router-dom'
+import Quiz from './containers/Quiz/Quiz.js'
+import QuizList from './containers/QuizList/QuizList.js'
+import Auth from './containers/Auth/Auth.js'
+import QuizCreator from './containers/QuizCreator/QuizCreator.js'
+import {connect} from 'react-redux'
+import Logout from './components/Logout/Logout.js'
+import {autoLogin} from './store/actions/auth.js'
+
+class App extends Component {
+
+    componentDidMount() {
+        this.props.autoLogin()
+    }
+
+    render() {
+
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth}/>                
+                <Route path="/quiz/:id" component={Quiz}/>
+                <Route path="/" exact component={QuizList}/>
+                <Redirect to={'/'} />
+            </Switch>
+        )
+
+        if (this.props.isAuthenticated) {
+            routes = (
+            <Switch>
+                <Route path="/quiz-creator" component={QuizCreator}/>
+                <Route path="/quiz/:id" component={Quiz}/>
+                <Route path="/logout" component={Logout} />
+                <Route path="/" exact component={QuizList}/>                
+                <Redirect to={'/'} />
+            </Switch>   
+            )
+        }
+
+        return (
+            <Layout>
+                {routes}
+            </Layout>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticated: !!state.auth.token
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        autoLogin: () => dispatch(autoLogin())
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
